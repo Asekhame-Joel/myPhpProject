d1ca6cdf528b855bf212fbb2b2290cea8dd97d72
asekhame <asekhamejoel@gmail.com>
2024-03-13 08:37:38 +0100

    more feature added

--- core/App.php
+++ core/App.php
@@ -10,25 +10,20 @@ class App{
         static::$container = $container;
     }
 
-
     public static function container(){
 
             return static::$container;
      }
          
-
        
        public static function bind($key, $resolver){
         static::container()->bind($key, $resolver);
        }
-
-
 
        public static function resolve($key){
         return static::container()->resolve($key);
        }
    
 }
-        
 require base_path('views/notes/bootstrap.php');
 
